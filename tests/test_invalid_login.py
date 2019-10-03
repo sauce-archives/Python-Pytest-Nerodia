b18088b0778ef4bbf6b7a750775469e57f63c64c
joshgrant <joshua.m.grant@gmail.com>
2019-10-03 10:58:57 -0400

    updated to latest versions of browsers

--- tests/test_invalid_login.py
+++ tests/test_invalid_login.py
@@ -10,3 +10,18 @@ def test_locked_out_user(browser):
 
     assert browser.button(class_name='error-button').exists
     
+def test_blank_user(browser):
+    browser.goto('http://www.saucedemo.com')
+
+    browser.button(type='submit').click()
+
+    assert browser.button(class_name='error-button').exists
+
+def test_invalid_credentials(browser):
+    browser.goto('http://www.saucedemo.com')
+
+    browser.text_field(data_test='username').value = 'bad'
+    browser.text_field(data_test='password').value ='bad'
+    browser.button(type='submit').click()
+
+    assert browser.button(class_name='error-button').exists
